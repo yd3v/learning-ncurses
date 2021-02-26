2c851c17011e115a13a5d7b813022a6e9a29fe92
yd3v <ythalyson@gmail.com>
2021-02-26 10:53:05 -0300

    let's see the colors !

--- colors.c
+++ colors.c
@@ -1,19 +1,22 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
 	int window_height = 20;
-	int window_width = 50;
+	int window_width = 60;
+	char *text = "MOVE-ME TO SEE THE COLORS :D";
 	time_t t;
+
 	srand((unsigned)time(&t));
 
 	initscr();
 	noecho();
 
 	WINDOW *win = newwin(window_height, window_width, 0, 0);
-	WINDOW *stats = newwin(7, 50, window_height, 0);
+	WINDOW *stats = newwin(7, window_width, window_height, 0);
 
 	box(win, 0, 0);
 	refresh();
@@ -31,7 +34,7 @@ int main(int argc, char *argv[]) {
 
 	start_color(); // This is necessary to work with colors
 
-	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	init_pair(1, COLOR_BLACK, COLOR_WHITE); // Init a color pair to use as attr
 	init_pair(2, COLOR_BLACK, COLOR_RED);
 	init_pair(3, COLOR_BLACK, COLOR_GREEN);
 	init_pair(4, COLOR_BLACK, COLOR_MAGENTA);
@@ -41,17 +44,30 @@ int main(int argc, char *argv[]) {
 
 	init_pair(8, COLOR_RED, COLOR_WHITE);
 	init_pair(9, COLOR_RED, COLOR_YELLOW);
-	init_pair(10, COLOR_RED, COLOR_BLUE);
+	init_pair(10, COLOR_RED, COLOR_GREEN);
+	init_pair(11, COLOR_RED, COLOR_CYAN);
+	init_pair(12, COLOR_RED, COLOR_BLUE);
+	init_pair(13, COLOR_RED, COLOR_MAGENTA);
+	init_pair(14, COLOR_RED, COLOR_BLACK);
 
-	wattron(win, COLOR_PAIR(1));
+
+	init_pair(15, COLOR_GREEN, COLOR_WHITE);
+	init_pair(16, COLOR_GREEN, COLOR_YELLOW);
+	init_pair(17, COLOR_GREEN, COLOR_RED);
+	init_pair(18, COLOR_GREEN, COLOR_CYAN);
+	init_pair(19, COLOR_GREEN, COLOR_BLUE);
+	init_pair(20, COLOR_GREEN, COLOR_MAGENTA);
+	init_pair(21, COLOR_GREEN, COLOR_BLACK);
+
+	wattron(win, COLOR_PAIR(1)); // Apply the attr
 	mvwprintw(win, 1, 3, "now let's learn how to use colors");
-	wattroff(win, COLOR_PAIR(1));
+	wattroff(win, COLOR_PAIR(1)); // Remove the attr
 
 	move(5, 3);
 	wrefresh(win);
 	int y_index = (int)(window_height/2);
 
-	int x_index = (int)(window_width/2);
+	int x_index = (int)(window_width/2)-(int)(strlen(text)/2)-2;
 
 	int ch;
 
@@ -60,7 +76,7 @@ int main(int argc, char *argv[]) {
 		ch = getch();
 
 		int randint;
-		randint = (rand() % 10); // Generate the random number at start of the while
+		randint = (rand() % 21)+1;
 
 		if(y_index == 1 && ch == 65) {
 			y_index = window_height-1;
@@ -69,9 +85,9 @@ int main(int argc, char *argv[]) {
 		}
 
 
-		if(x_index == 1 && ch == 68) {
+		if(x_index <= 1 && ch == 68) {
 			x_index = window_width-1;
-		} else if (x_index == window_width - 2 && ch == 67) {
+		} else if (x_index >= window_width - 1 && ch == 67) {
 			x_index = 0;
 		}
 
@@ -85,10 +101,10 @@ int main(int argc, char *argv[]) {
 				y_index+=1;
 				break;
 			case 67:
-				x_index+=1;
+				x_index+=2;
 				break;
 			case 68:
-				x_index-=1;
+				x_index-=2;
 				break;
 			default:
 				continue;
@@ -98,8 +114,8 @@ int main(int argc, char *argv[]) {
 		wclear(win);
 		wclear(stats);
 
-		mvwprintw(stats, 1, 2, "color pair  	 :");
-		mvwprintw(stats, 3, 2, "key pressed 	 :");
+		mvwprintw(stats, 1, 2, "color pair        :");
+		mvwprintw(stats, 3, 2, "key pressed       :");
 		mvwprintw(stats, 5, 2, "x / y coordinates :");
 
 		wattron(stats, COLOR_PAIR(randint));
@@ -108,10 +124,9 @@ int main(int argc, char *argv[]) {
 		mvwprintw(stats, 5, 25, "%d, %d", x_index, y_index);
 		wattroff(stats, COLOR_PAIR(randint));
 
-		wmove(win, y_index, x_index);
-
+//		wmove(win, y_index, x_index);
 		wattron(win, COLOR_PAIR(randint));
-		wprintw(win, "MOVE-ME");
+		mvwprintw(win, y_index, x_index, text);
 		wattroff(win, COLOR_PAIR(randint));
 
 		box(win, 0, 0);
